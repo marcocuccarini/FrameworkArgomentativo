7957763e2cc043d11bf3eee38018d73976be86e2
Marco Cuccarini <59500902+marcocuccarini@users.noreply.github.com>
2025-04-28 20:20:35 +0200

    VerioReady to test

--- argumentative_graph/3_sentence_encoder.py
+++ argumentative_graph/3_sentence_encoder.py
@@ -5,24 +5,34 @@ import numpy as np
 
 def encode_json(model, json_file):
     """Encodes the textual arguments using the provided model."""
-    for item in json_file:
-        for argument in item.get('arguments', []):
+    for item in json_file[:2]:
+
+        print(item['_id'])
+
+        for argument in item.get('arguments', [])[:2]:
+
+
+            #print(argument)
+
             # Convert ndarray to list for JSON serialization
             argument['sentences_encode'] = model.text_embedding(argument['sentences']).tolist()
 
     return json_file
 
 def extract_json(path):
+
     """Extracts JSON data from a given file path."""
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 def save_json(data, path):
+
     """Saves JSON data to a specified file path."""
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
+
     # Load the dataset
     json_file = extract_json("arg_dic_supp_augmented.json")
 
